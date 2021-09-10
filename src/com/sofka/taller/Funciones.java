0d9f53f9d85a694426c25cc338b45b158093e1ee
jocarito <jcriostorres@gmail.com>
2021-09-09 22:35:38 -0500

    creacion de funciones del 4 al 8

--- src/com/sofka/taller/Funciones.java
+++ src/com/sofka/taller/Funciones.java
@@ -30,15 +30,87 @@ public class Funciones {
             System.out.println("los numeros son iguales");
     }
 
-    ///funcion 3
+    //funcion 3
     public static void areaCirculo(double radio) {
+        double area = Math.PI * radio * radio;
+        System.out.println("El area del circulo es: " + area);
+    }
 
-           double area = Math.PI * radio * radio;
+//funcion4
 
-            System.out.println("El area del circulo es " + area);
+    public static void precioFinalConIva(double precioProducto) {
+        double IVA = 0.21;
+        precioProducto = precioProducto + (precioProducto * IVA);
 
+        System.out.println("El precio total incluido Iva es: " + precioProducto);
+
+    }
+//funcion 5
+
+    public static void mostrarParesEimparesConWhile() {
+        int i = 1;
+        while (i <= 100) {
+            if (i % 2 == 0)
+                System.out.println(i + " es par");
+            else
+                System.out.println(i + " es impar");
+
+            i++;
         }
     }
+
+    //funcion 6
+
+    public static void mostrarParesEimparesConFor() {
+
+        for (int i = 0; i <= 100; i++)
+            if (i % 2 == 0)
+                System.out.println(i + " es par");
+            else
+                System.out.println(i + " es impar");
+    }
+
+
+    //funcion 7
+    public static void numeroMayorACero(int num) {
+        num = -1;
+        while (num < 0) ;
+        try {
+            //num = Integer.parseInt(sc.nextLine());
+            if (num == 0) throw new Exception();
+        } catch (NumberFormatException e) {
+            System.out.println("Tienes que introducir números");
+            num = -1;
+        } catch (Exception e) {
+            System.out.println("Tienes que introducir números positivos mayores que cero");
+            num = -1;
+        }
+    }
+
+
+    // funcion 8
+    public static String diaEsLaboralONoLaboral(String dia) {
+        String esLaboralONo = "";
+
+        switch (dia) {
+            case "lunes":
+            case "martes":
+            case "miercoles":
+            case "jueves":
+            case "viernes":
+                System.out.println("Es un dia laboral");
+                break;
+            case "sabado":
+            case "domingo":
+                System.out.println("Es un dia festivo");
+            default:
+                System.out.println("Introduce un dia de la semana");
+        }
+        return esLaboralONo;
+    }
+
+}
+
 
 
 
--- src/com/sofka/taller/Main.java
+++ src/com/sofka/taller/Main.java
@@ -16,22 +16,22 @@ public class Main {
             System.out.println("1. Numero Mayor  ");
             System.out.println("2. Numero Mayor por consola  ");
             System.out.println("3. Area del circulo ");
-            System.out.println("4.  Precio final ");
-            System.out.println("5.  Mostar numero impare y pares ");
-            System.out.println("6.  Mostar numero impare y pares con ccilo for ");
-            System.out.println("7.  Numero mayor o igaul que cero ");
-            System.out.println("8.  es di laboral  ");
-            System.out.println("9.  Reemplazar y concatenar ");
-            System.out.println("10.  Eliminar espacios ");
+            System.out.println("4. Precio final ");
+            System.out.println("5. Mostar numero impare y pares ");
+            System.out.println("6. Mostar numero impare y pares con ccilo for ");
+            System.out.println("7. Numero mayor o igual que cero ");
+            System.out.println("8. Es dia laboral  ");
+            System.out.println("9. Reemplazar y concatenar ");
+            System.out.println("10. Eliminar espacios ");
             System.out.println("11. Longitud de la frase y contar vocales  ");
-            System.out.println("12.  Palabras iguales ");
-            System.out.println("13.  cpnsultar fecha y hora ");
-            System.out.println("14.  Nuemros con saldos de 2 en 2 ");
-            System.out.println("15.  Menu Cinematografia ");
-            System.out.println("16.  ejercicio 16 ");
-            System.out.println("17.  Ejercicio 17 ");
-            System.out.println("17.  Ejercicio 18 ");
-            System.out.println("18.  Salir ");
+            System.out.println("12. Palabras iguales ");
+            System.out.println("13. cpnsultar fecha y hora ");
+            System.out.println("14. Numeros con saldos de 2 en 2 ");
+            System.out.println("15. Menu Cinematografia ");
+            System.out.println("16. Ejercicio 16 ");
+            System.out.println("17. Ejercicio 17 ");
+            System.out.println("17. Ejercicio 18 ");
+            System.out.println("18. Salir ");
 
             int option = leer.nextInt();
 
@@ -53,22 +53,46 @@ public class Main {
                 case 3:
                     System.out.println("3. Area del circulo ");
                     System.out.println("Escribe valor del radio: ");
-                    double entrada = leer.nextInt();
-                    //doble radio = Double.parseDouble(entrada);
-
+                    double entrada = leer.nextDouble();
+                    //double radio = Double.parseDouble(entrada);
                     areaCirculo(entrada);
                     break;
 
-
-
-
-
                 case 4:
+                    System.out.println("Ingrese el precio del producto: ");
+                    double entrada2 = leer.nextDouble();
+                    precioFinalConIva(entrada2);
                     break;
+
                 case 5:
+                    System.out.println(" Mostar numero impare y pares:  ");
+                    mostrarParesEimparesConWhile();
+                    break;
+
+
                 case 6:
+                    System.out.println(" Mostar numero impare y pares:  ");
+                    mostrarParesEimparesConFor();
+                    break;
+
+
                 case 7:
+                    System.out.println(" Numero mayor o igual que cero:  ");
+                    System.out.print("Ingrese un numero mayor que cero:  ");
+                    int entrada3 = leer.nextInt();
+
+                    numeroMayorACero(entrada3);
+                    break;
+
+
+
                 case 8:
+                    System.out.println("Es dia laboral? ");
+                    System.out.print("Ingrese un dia de la semana: ");
+                    String entrada4 = leer.next();
+                    //System.out.println(diaEsLaboralONoLaboral(entrada4));
+                    diaEsLaboralONoLaboral(entrada4);
+                    break;
                 case 9:
                 case 10:
                 case 11:
